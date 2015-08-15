02a0dfa45766060ba07378d69f13e02ceea3951a
e-lulu <lucie.vigreux@laposte.net>
2015-08-15 18:32:04 +0200

    Update RK4-3D.js

--- RK4-3D.js
+++ RK4-3D.js
@@ -41,47 +41,47 @@ dt = 1/30;     //timestep 1/30
 
 //acceleration function returns the acceleration for a timestep, position and velocity
 function acceleration (x,y,vx,vy,i,dt) {
-	a[i].x = 0;
-	a[i].y = -g * y; 	      //just gravity, no air friction for the moment
-	return dt;
+    a[i].x = 0;
+    a[i].y = -g * y; 	      //just gravity, no air friction for the moment
+    return dt;
 }
 
 //return final position and velocity after time dt
 function rk(x,y,vx,vy,dt) {
-	var x1,x2,x3,x4;           //the x position of the 4 steps 
-	var vx1,vx2,vx3,vx4;       //the vx velocity of the 4 steps
-	var y1,y2,y3,y4;           //the y position of the 4 steps 
-	var vy1,vy2,vy3,vy4;       //the vy velocity of the 4 steps
+    var x1,x2,x3,x4;           //the x position of the 4 steps 
+    var vx1,vx2,vx3,vx4;       //the vx velocity of the 4 steps
+    var y1,y2,y3,y4;           //the y position of the 4 steps 
+    var vy1,vy2,vy3,vy4;       //the vy velocity of the 4 steps
 	
-	x1 = x;
-	y1 = y;
-	vx1 = vx;
-	vy1 = vy;
-	acceleration(x1,y1,vx1,vy1,1,0);    //init dt =0
+    x1 = x;
+    y1 = y;
+    vx1 = vx;
+    vy1 = vy;
+    acceleration(x1,y1,vx1,vy1,1,0);    //init dt =0
 	
-	x2 = x + 0.5 * vx1 * dt;
-	y2 = x + 0.5 * vy1 * dt;
-	vx2 = vx + 0.5 * a[1].x * dt;
-	vy2 = vy + 0.5 * a[1].y * dt;
-	acceleration(x2,y2,vx2,vy2,2,dt/2);
+    x2 = x + 0.5 * vx1 * dt;
+    y2 = x + 0.5 * vy1 * dt;
+    vx2 = vx + 0.5 * a[1].x * dt;
+    vy2 = vy + 0.5 * a[1].y * dt;
+    acceleration(x2,y2,vx2,vy2,2,dt/2);
 	
-	x3 = x + 0.5 * vx2 * dt;
-	y3 = y + 0.5 * vy2 * dt;
-	vx3 = vx + 0.5 * a[2].x * dt;
-	vy3 = vy + 0.5 * a[2].y * dt;
-	acceleration(x3,y3,vx3,vy3,3,dt/2);
+    x3 = x + 0.5 * vx2 * dt;
+    y3 = y + 0.5 * vy2 * dt;
+    vx3 = vx + 0.5 * a[2].x * dt;
+    vy3 = vy + 0.5 * a[2].y * dt;
+    acceleration(x3,y3,vx3,vy3,3,dt/2);
 	
-	x4 = x + vx3 * dt;
-	y4 = y + vx3 * dt;
-	vx4 = vx + a[3].x * dt;
-	vy4 = vy + a[3].y * dt;
-	acceleration(x4,y4,vx4,vy4,4,dt);
+    x4 = x + vx3 * dt;
+    y4 = y + vx3 * dt;
+    vx4 = vx + a[3].x * dt;
+    vy4 = vy + a[3].y * dt;
+    acceleration(x4,y4,vx4,vy4,4,dt);
 	
-	finalState.xf = x + (dt/6) * (vx1 + 2 * vx2 + 2 * vx3 + vx4);                 //final position xf, NB: x = dvx/dt
-	finalState.yf = y + (dt/6) * (vy1 + 2 * vy2 + 2 * vy3 + vy4);                 //final position yf, NB: y = dvy/dt
+    finalState.xf = x + (dt/6) * (vx1 + 2 * vx2 + 2 * vx3 + vx4);                 //final position xf, NB: x = dvx/dt
+    finalState.yf = y + (dt/6) * (vy1 + 2 * vy2 + 2 * vy3 + vy4);                 //final position yf, NB: y = dvy/dt
 	
-	finalState.vxf = vx + (dt/6) * (a[1].x + 2 * a[2].x + 2 * a[3].x + a[4].x);   //final velocity vxf
-	finalState.vyf = vy + (dt/6) * (a[1].y + 2 * a[2].y + 2 * a[3].y + a[4].y);   //final velocity vyf
+    finalState.vxf = vx + (dt/6) * (a[1].x + 2 * a[2].x + 2 * a[3].x + a[4].x);   //final velocity vxf
+    finalState.vyf = vy + (dt/6) * (a[1].y + 2 * a[2].y + 2 * a[3].y + a[4].y);   //final velocity vyf
 }
 
 //main-like function 
